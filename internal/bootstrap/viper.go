c090c6ddbcc262f68ea0889eafb24b45beb49982
Hanz Jefferson <hanzjefferson.dev@gmail.com>
2026-07-01 12:50:12 +0700

    feat: setup default configuration

--- internal/bootstrap/viper.go
+++ internal/bootstrap/viper.go
@@ -13,6 +13,12 @@ func NewViper() *viper.Viper {
 	v.SetConfigType("yaml")
 	v.AddConfigPath("./")
 
+	v.SetDefault("app.debug", true)
+	v.SetDefault("app.name", "go-application")
+	v.SetDefault("server.host", "localhost")
+	v.SetDefault("server.port", 3000)
+	v.SetDefault("database.driver", "mysql")
+
 	if err := v.ReadInConfig(); err != nil {
 		panic(fmt.Errorf("viper couldn't read config:\n%v", err))
 	}
